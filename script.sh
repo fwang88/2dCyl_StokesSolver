bcb9ee5072cab42fb6b634d187dcf3b6c23641bb
Fan Wang <wangfan@mit.edu>
2015-01-15 18:06:05 -0500

    revise the script

--- script.sh
+++ script.sh
@@ -1,12 +1,30 @@
-LISTI="0.1"
-LISTJ="0.02"
-# 0.5 0.2 0.1 0.05 0.02 0.01"
-for i in $LISTI
+LISTTW="910"
+LISTVEL="1"
+LISTThigh="1800"
+LISTTlow="1400"
+dr=0.1
+dz=0.1
+maxz=100
+maxr=10
+pertb=0.02
+lowtwidth=100
+trestart=0
+restart=0
+initflag=0
+temp_profile=0
+
+for Thigh in $LISTThigh
 do
-for j in $LISTJ 
+for Tlow in $LISTTlow
 do
-${PETSC_DIR}/${PETSC_ARCH}/bin/mpirun -np 2 ./stokes --dr=$i --pertb=$j --dt=0.001 --outputstep=0.001 --maxZ=20 --dz=$i --maxR=10 --r0=0.97 --tension=1.0 --initflag=0
-echo $i
+for tw in $LISTTW
+do
+for vel in $LISTVEL
+do
+outputstep=`echo "scale=4; $dr/$vel*20.0" | bc`
+${PETSC_DIR}/${PETSC_ARCH}/bin/mpirun -np 4 ./stokes --maxR=$maxr --maxZ=$maxz --dr=$dr --dz=$dz --r0=0.97 --tension=1.0 --Twidth=$tw --Tlow=$Tlow --Thigh=$Thigh --vf=$vel --outputstep=$outputstep --pertb=$pertb --LowTWidth=$lowtwidth --trestart=$trestart --restart=$restart --initflag=0 --temp_profile=$temp_profile
+done
+done
 done
 done
 
