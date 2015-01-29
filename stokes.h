8a890c949f25d8d07424afbfe763cb68df68d3c1
Fan Wang <wangfan@mit.edu>
2015-01-29 00:01:00 -0500

    delete old functions in header file stokes.h

--- stokes.h
+++ stokes.h
@@ -131,21 +131,4 @@ void stokes_solve(stokes_force *uwp,
                   parameter *para,
                   viscosity *mu);
 
-
-PETSC_EXTERN void RK2DReinit(Vec, PetscInt, PetscScalar, PetscScalar, PetscInt, PetscInt, DM);
-
-PETSC_EXTERN void Euler_Reinit_2D(Vec, Vec, PetscScalar, PetscScalar, PetscScalar, PetscScalar, PetscInt, PetscInt, DM);
-
-PETSC_EXTERN void Euler_mex2D(DM, DM, Vec, Vec, Vec, PetscScalar,  PetscScalar, PetscInt, PetscInt, PetscScalar);
-
-PETSC_EXTERN void RK2DPeriod(DM, DM, Vec, Vec, PetscScalar, PetscScalar, PetscInt, PetscInt, PetscScalar );
-
-PETSC_EXTERN void AssembleB( Mat, DM, DM , PetscScalar , PetscInt , PetscInt , PetscScalar , PetscScalar , Vec , PetscScalar *, PetscScalar *);
-
-void AssembleForce(Vec , Vec , DM , DM , PetscInt , PetscInt , PetscScalar , PetscScalar , PetscScalar, PetscScalar );
-
-double timestep(DM, Vec, PetscScalar, PetscScalar, PetscScalar);
-
-
-
 #endif
